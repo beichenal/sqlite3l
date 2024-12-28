9a442800c547b05d834ea715172025880b153094
dengliangdong <1615527807@qq.com>
2024-12-28 18:20:46 +0800

    save

--- src/main/db/server.ts
+++ src/main/db/server.ts
@@ -1,11 +1,80 @@
 import { ServerInterface } from './types';
-import {Database} from ''
+import { Database, Statement } from 'better-sqlite3';
+import SQL from 'better-sqlite3-multiple-ciphers';
+import { ensureDirSync, removeSync } from 'fs-extra';
+import { consoleLogger } from 'App/utils/consoleLogger';
+import { getSchemaVersion, getUserVersion, setUserVersion } from './util';
 
 const user_id_key = 1;
 
 let globalInstance: Database | undefined;
 const logger = consoleLogger;
 let databaseFilePath: string | undefined;
+type DatabaseQueryCache = Map<string, Statement<Array<unknown>>>;
+const statementCache = new WeakMap<Database, DatabaseQueryCache>();
+
+function prepare<T extends unknown[]>(db: Database, query: string): Statement<T> {
+  let dbCache = statementCache.get(db);
+  if (!dbCache) {
+    dbCache = new Map();
+    statementCache.set(db, dbCache);
+  }
+
+  let result = dbCache.get(query) as Statement<T>;
+
+  if (!result) {
+    result = db.prepare<T>(query);
+    dbCache.set(query, result);
+  }
+  return result;
+}
+
+function keyDatabase(db: Database, key: string): void {
+  db.pragma(`cipher='sqlcipher'`);
+  db.pragma(`legacy=4`);
+  db.pragma(`key='${key}'`);
+}
+
+function switchToWAL(db: Database): void {
+  // https://sqlite.org/wal.html
+  db.pragma('journal_mode = WAL');
+  db.pragma('synchronous = FULL');
+  db.pragma('fullfsync = ON');
+}
+
+function migrateSchemaVersion(db: Database): void {
+  const userVersion = getUserVersion(db);
+
+  if (userVersion > 0) return;
+
+  const schemaVersion = getSchemaVersion(db);
+  const newUserVersion = schemaVersion;
+  logger.info(
+    'migrateSchemaVersion: Migrating from schema_version ' +
+      `${schemaVersion} to user_version ${newUserVersion}`
+  );
+
+  setUserVersion(db, newUserVersion);
+}
+
+function openAndMigrateDatabase(filePath: string, key: string): Database | undefined {
+  let db: Database | undefined;
+
+  try {
+    db = new SQL(filePath);
+    keyDatabase(db, key);
+    switchToWAL(db);
+    migrateSchemaVersion(db);
+
+    return db;
+  } catch (error) {
+    logger.error(error);
+    if (db) db.close();
+
+    logger.info('migrateDatabase: Migration without cipher change failed');
+    throw new Error('migrateDatabase: Migration without cipher change failed');
+  }
+}
 
 const dataInterface: ServerInterface = {
   close,
@@ -18,5 +87,34 @@ const dataInterface: ServerInterface = {
 };
 
 async function close(): Promise<void> {
-  globalInstance?.
+  globalInstance?.pragma('optimize');
+  globalInstance?.close();
+  globalInstance = undefined;
+}
+
+async function removeDB(): Promise<void> {
+  if (globalInstance) {
+    try {
+      globalInstance.close();
+    } catch (error) {
+      logger.error('removeDB: Failed to close database:', error.stack);
+    }
+    globalInstance = undefined;
+  }
+
+  if (!databaseFilePath)
+    throw new Error('removeDB: Cannot erase database without a databaseFilePath!');
+
+  logger.warn('removeDB: Removing all database files');
+  removeSync(databaseFilePath);
+  removeSync(`${databaseFilePath}-shm`);
+  removeSync(`${databaseFilePath}-wal`);
+}
+
+function getInstance(): Database {
+  if (!globalInstance) {
+    throw new Error('getInstance: globalInstance not set!');
+  }
+
+  return globalInstance;
 }
---
+++ src/main/db/util.ts
@@ -0,0 +1,39 @@
+import type { Database } from 'better-sqlite3';
+import { isNumber } from 'lodash';
+// This value needs to be below SQLITE_MAX_VARIABLE_NUMBER.
+// const MAX_VARIABLE_COUNT = 100;
+
+export function objectToJSON<T>(data: T): string {
+  return JSON.stringify(data);
+}
+
+export function jsonToObject<T>(json: string): T {
+  return JSON.parse(json);
+}
+
+export function getSQLiteVersion(db: Database): string {
+  const { sqlite_version: version } = db
+    .prepare('select sqlite_version() AS sqlite_version')
+    .get() as { sqlite_version: string };
+
+  return version;
+}
+
+export function getSchemaVersion(db: Database): number {
+  return db.pragma('schema_version', { simple: true }) as number;
+}
+
+export function setUserVersion(db: Database, version: number): void {
+  if (!isNumber(version)) {
+    throw new Error(`setUserVersion: version ${version} is not a number`);
+  }
+  db.pragma(`user_version = ${version}`);
+}
+
+export function getUserVersion(db: Database): number {
+  return db.pragma('user_version', { simple: true }) as number;
+}
+
+export function getSQLCipherVersion(db: Database): unknown {
+  return db.pragma('cipher_version', { simple: true });
+}
